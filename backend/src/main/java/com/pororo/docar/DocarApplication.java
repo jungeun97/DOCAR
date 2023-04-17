8ea5cc8d8bb6e7574c2a123195a1984a39a8fb20
kavin816@naver.com <kavin816@naver.com>
2023-04-17 10:44:12 +0900

    chore: git submodule 등록

---
+++ backend/src/main/java/com/pororo/docar/DocarApplication.java
@@ -0,0 +1,13 @@
+package com.pororo.docar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocarApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/pororo/docar/DocarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pororo.docar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
