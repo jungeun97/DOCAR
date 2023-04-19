fc86a284832f96e04c8a87923a7f8ce036c6355b
haetsal <leeee99@hanmail.net>
2023-04-19 17:58:37 +0900

    feat: 책 반납페이지 css 작업

--- frontend/src/Presentational/Components/ReturnDetail_Style.tsx
+++ frontend/src/Presentational/Components/ReturnDetail_Style.tsx
@@ -1,18 +1,41 @@
 import styled from 'styled-components';
 
-export const BookDiv = styled.div`
-  display: flex;
-`;
-export const BookTextDiv = styled.div`
+export const WrapDetail = styled.div`
+  width: 100%;
+  height: 100%;
   display: flex;
   flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const BookDiv = styled.div`
+  display: flex;
+  margin-bottom: 3rem;
 `;
 
 export const BookImg = styled.img`
-  width: 100px;
-  height: 150px;
+  width: 10rem;
+  height: 15rem;
 `;
 
-export const BookName = styled.span``;
+export const BookTextDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 15rem;
+  padding: 20px;
+  justify-content: center;
+  text-align: center;
+`;
+export const BookName = styled.span`
+  font-size: 1.2rem;
+  font-weight: bold;
+  margin-bottom: 2rem;
+`;
 
 export const BookWriter = styled.span``;
+
+export const DetailText = styled.div`
+  font-size: 1.5rem;
+  font-weight: bold;
+`;
--- frontend/src/Presentational/Components/ReturnList_Style.tsx
+++ frontend/src/Presentational/Components/ReturnList_Style.tsx
@@ -1,1 +1,52 @@
 import styled from 'styled-components';
+
+export const WrapList = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const ListDiv = styled.div`
+  height: 250px;
+  overflow-y: scroll;
+`;
+
+export const ListTitle = styled.div`
+  font-size: 1.5rem;
+  font-weight: bold;
+  text-align: center;
+`;
+
+export const BookDiv = styled.div`
+  display: flex;
+  margin-bottom: 1rem;
+`;
+
+export const BookImg = styled.img`
+  width: 3rem;
+  height: 4rem;
+  margin-right: 0.5rem;
+`;
+
+export const BookTextDiv = styled.div`
+  display: flex;
+  /* flex-direction: column; */
+  /* width: 20rem; */
+  padding: 5px;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const BookName = styled.div`
+  font-size: 1rem;
+  margin-right: 0.5rem;
+  text-align: center;
+`;
+
+export const BookWriter = styled.div`
+  font-size: 0.75rem;
+  text-align: end;
+`;
