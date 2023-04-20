42660907a0a2c57619e18629f9e3331e8bd6ec3c
leejinsung <jsmon3536@gmail.com>
2023-04-20 17:19:42 +0900

    feat: 도서 반납, 정리 모달 로직 설계 완료 / 중첩라우터 해제

--- frontend/src/Presentational/Components/Cleanup/BookList_Style.tsx
+++ frontend/src/Presentational/Components/Cleanup/BookList_Style.tsx
@@ -2,6 +2,16 @@ import styled from "styled-components";
 
 export const WrapContent = styled.div``;
 
+export const WrapClean = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  position: relative;
+`;
+
 export const WrapBooks = styled.div`
   width: 80%;
   height: 70%;
--- frontend/src/Presentational/Components/Cleanup/Bookshelf.tsx
+++ frontend/src/Presentational/Components/Cleanup/Bookshelf.tsx
@@ -5,7 +5,7 @@ import * as BookStyle from './BookList_Style';
 import BottomBtn from './../../Common/BottomBtn';
 import ResetBtn from './../../Common/ResetBtn';
 import Btn from './../../Common/Btn';
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate, Link } from 'react-router-dom';
 import Swal from 'sweetalert2';
 import withReactContent from 'sweetalert2-react-content';
 import BookTable from './../BookTable';
@@ -17,58 +17,121 @@ type recipeParams = {
 
 function Bookshelf() {
   const [books, setBooks] = useState<[]>([]);
-  const [number, setNumber] = useState();
   const { id } = useParams() as { id: string };
+
+  const navigate = useNavigate();
 
   const GetBooks = () => {
     axios.get('https://jsonplaceholder.typicode.com/todos').then((res) => {
       setBooks(res.data);
+      console.log(res.data);
     });
   };
+
   const MySwal = withReactContent(Swal);
 
   // 다음 책장으로 가는 모달
   const setModal = () => {
     MySwal.fire({
-      icon: 'success',
-      title: '책장 정리 완료!',
+      title: '모든 책을 정리하셨습니까?',
       // timer: 1500,
       timerProgressBar: true,
       // showConfirmButton: false,
       showCancelButton: true,
       showConfirmButton: true,
-      cancelButtonText: '원위치로',
-      confirmButtonText: '다음 책장으로 이동',
-      html: <div>""</div>,
+      cancelButtonText: '아니오',
+      confirmButtonText: '네',
+      allowOutsideClick: false,
+      html: <div></div>,
+    }).then((result) => {
+      if (result.isConfirmed) {
+        // 확인 버튼을 눌렀을 때,
+        MySwal.fire({
+          icon: 'success',
+          title: '책장 정리 완료!',
+          // timer: 1500,
+          timerProgressBar: true,
+          // showConfirmButton: false,
+          showCancelButton: true,
+          showConfirmButton: true,
+          cancelButtonText: '정리 종료',
+          confirmButtonText: '다음 책장으로 이동',
+          allowOutsideClick: false,
+          html: <div></div>,
+        }).then((result) => {
+          if (result.isConfirmed) {
+            // 확인 버튼을 눌렀을 때,
+            // 확인 버튼을 누르면 axios로 다음 책장을 받는다.
+            console.log(result); // 다음 책장이 있으면 다음 페이지로 넘어가고, 없으면 마무리 페이지로 간다.
+            axios
+              .get('https://jsonplaceholder.typicode.com/todos')
+              .then((res) => {
+                // setBooks(res.data);
+                console.log('도서 정리 완료 요청 성공');
+                navigate(`/cleanup/2`);
+              });
+          } else {
+            navigate(`/cleanup/end`);
+          }
+        });
+      } else {
+      }
     });
   };
 
   // 중간에 탈주하는 모달
   const setCheckModal = () => {
     MySwal.fire({
-      // icon: 'success',
-      title: '정리된 책들을 체크해주세요',
-      // timer: 1500,
+      title: '현재 정리된 책을 체크해주세요',
       timerProgressBar: true,
-      // showConfirmButton: false,
       showCancelButton: true,
       showConfirmButton: true,
-      // width: '1000px',
-      // height: '400px',
       cancelButtonText: '취소',
-      confirmButtonText: '반납 및 이동',
+      confirmButtonText: '반납하기',
+      allowOutsideClick: false,
       html: (
         <>
-          {/* {books && (
-            <BookStyle.WrapBooks>
-              {books.map((book) => (
-                <BookItem book={book} />
-              ))}
-            </BookStyle.WrapBooks>
-          )} */}
           <BookTableBack />
         </>
       ),
+    }).then((result) => {
+      if (result.isConfirmed) {
+        // 확인 버튼을 눌렀을 때,
+        // 확인 버튼을 누르면 axios로 다음 책장을 받는다.
+        MySwal.fire({
+          title: '반납된 책이 맞습니까?',
+          timerProgressBar: true,
+          showCancelButton: true,
+          showConfirmButton: true,
+          cancelButtonText: '아니오',
+          confirmButtonText: '네',
+          allowOutsideClick: false,
+          html: (
+            <>
+              <BookTableBack />
+            </>
+          ),
+        }).then((result) => {
+          if (result.isConfirmed) {
+            // 확인 버튼을 눌렀을 때,
+            // 확인 버튼을 누르면 axios로 다음 책장을 받는다.
+            console.log(result); // 다음 책장이 있으면 다음 페이지로 넘어가고, 없으면 마무리 페이지로 간다.
+            axios
+              .get('https://jsonplaceholder.typicode.com/todos')
+              .then((res) => {
+                // setBooks(res.data);
+                console.log('도서 정리 완료 요청 성공');
+                navigate(`/cleanup/end`);
+              });
+          } else {
+            setCheckModal();
+          }
+        });
+      } else {
+        MySwal.fire({
+          title: '실팽',
+        });
+      }
     });
   };
 
@@ -77,9 +140,9 @@ function Bookshelf() {
   }, []);
 
   return (
-    <>
+    <BookStyle.WrapClean>
       <ResetBtn
-        message="원위치로"
+        message="종료하기"
         go={() => {
           setCheckModal();
         }}
@@ -93,15 +156,16 @@ function Bookshelf() {
         </BookStyle.WrapBooks>
       )} */}
       <BookTable />
+
       {/* 여기서 이동할 때 이동해야할 책장 경로 */}
       <BottomBtn
-        message={`정리 완료`}
+        message={`정리완료`}
         go={() => {
           setModal();
         }}
       />
       {/* 모달 띄어서 다음 책장으로 이동시키기 */}
-    </>
+    </BookStyle.WrapClean>
   );
 }
 
