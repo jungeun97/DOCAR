f05d678e469089fc66372583e3f51ecf17212908
jungeun park <pyooooong@gmail.com>
2023-04-24 16:47:14 +0900

    feat: return post api 코드 작성

--- backend/src/main/java/com/pororo/docar/domain/tmpBook/service/TmpBookService.java
+++ backend/src/main/java/com/pororo/docar/domain/tmpBook/service/TmpBookService.java
@@ -1,6 +1,8 @@
 package com.pororo.docar.domain.tmpBook.service;
 
 import com.pororo.docar.domain.book.entity.Book;
+import com.pororo.docar.domain.cartBook.entity.CartBook;
+import com.pororo.docar.domain.cartBook.repository.CartBookRepository;
 import com.pororo.docar.domain.tmpBook.entity.TmpBook;
 import com.pororo.docar.domain.tmpBook.repository.TmpBookRepository;
 import lombok.RequiredArgsConstructor;
@@ -10,12 +12,29 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 
 @Service
-@Transactional(readOnly = true)
+@Transactional()
 @RequiredArgsConstructor
 public class TmpBookService {
     private final TmpBookRepository tmpBookRepository;
+    private final CartBookRepository cartBookRepository;
 
     public List<TmpBook> getTmpBooks() {
         return tmpBookRepository.findAll();
+    }
+
+    public void insertCartBooks() {
+        List<TmpBook> tmpBooks = tmpBookRepository.findAll();
+
+        for (TmpBook tmpBook : tmpBooks) {
+            CartBook cartBook = CartBook.builder()
+                    .floor(tmpBook.getFloor())
+                    .site(tmpBook.getSite())
+                    .book(tmpBook.getBook())
+//                    .user(tmpBook.getUser())
+                    .build();
+            cartBookRepository.save(cartBook);
+        }
+
+        tmpBookRepository.deleteAll();
     }
 }
