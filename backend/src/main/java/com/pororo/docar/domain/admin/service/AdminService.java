f557525e498527973aa6de7f34125e29c7230e4d
jungeun park <pyooooong@gmail.com>
2023-04-24 10:48:57 +0900

    feat: admin 생성

---
+++ backend/src/main/java/com/pororo/docar/domain/admin/service/AdminService.java
@@ -0,0 +1,13 @@
+package com.pororo.docar.domain.admin.service;
+
+import com.pororo.docar.domain.admin.repository.AdminRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class AdminService {
+    private final AdminRepository adminRepository;
+}
