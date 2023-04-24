a5ffe2ec9c0aaa34f14a40e501c407ef476034f5
haetsal <leeee99@hanmail.net>
2023-04-24 17:58:18 +0900

    feat: 버튼 css 작성중

--- frontend/src/Presentational/Pages/MainPage.tsx
+++ frontend/src/Presentational/Pages/MainPage.tsx
@@ -1,32 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import * as MainStyle from './MainPage_Style';
 import Btn from './../Common/Btn';
 import { useNavigate } from 'react-router-dom';
 
 function MainPage() {
   const navigate = useNavigate();
+  const [clicked, setClicked] = useState(false);
 
   const ToReturn = () => {
-    navigate(`/return`);
+    setClicked(true);
+    setTimeout(() => {
+      setClicked(false);
+      navigate(`/return`);
+    }, 2000);
   };
 
   const ToCleanup = () => {
-    navigate(`/login`);
+    setClicked(true);
+    setTimeout(() => {
+      setClicked(false);
+      navigate(`/login`);
+    }, 2000);
   };
 
   return (
     <MainStyle.WrapMain>
       {/* MainPage입니당 */}
-      <MainStyle.WrapBtn onClick={ToReturn}>
-        {/* <Btn message="반납" go={ToReturn} /> */}
-        {/* <MainStyle.Mbtn>반납</MainStyle.Mbtn> */}
-        반납
-      </MainStyle.WrapBtn>
-      <MainStyle.WrapBtn onClick={ToCleanup}>
-        {/* <MainStyle.Mbtn>정리</MainStyle.Mbtn> */}
-        정리
-        {/* <Btn message="정리" go={ToCleanup} /> */}
-      </MainStyle.WrapBtn>
+      <MainStyle.FlipWrapper>
+        <MainStyle.WrapBtn onClick={ToReturn}>
+          {/* <Btn message="반납" go={ToReturn} /> */}
+          {/* <MainStyle.Mbtn>반납</MainStyle.Mbtn> */}
+          반납
+        </MainStyle.WrapBtn>
+      </MainStyle.FlipWrapper>
+      <MainStyle.FlipWrapper>
+        <MainStyle.WrapBtn onClick={ToCleanup}>
+          {/* <MainStyle.Mbtn>정리</MainStyle.Mbtn> */}
+          정리
+          {/* <Btn message="정리" go={ToCleanup} /> */}
+        </MainStyle.WrapBtn>
+      </MainStyle.FlipWrapper>
     </MainStyle.WrapMain>
   );
 }
--- frontend/src/Presentational/Pages/MainPage_Style.tsx
+++ frontend/src/Presentational/Pages/MainPage_Style.tsx
@@ -1,5 +1,31 @@
 import styled from 'styled-components';
 import { AiFillLock } from 'react-icons/ai';
+import { Link } from 'react-router-dom';
+
+// export const WrapMain = styled.div`
+//   width: 100%;
+//   height: 100%;
+//   display: flex;
+//   justify-content: center;
+//   align-items: center;
+// `;
+
+// export const WrapBtn = styled.div`
+//   display: flex;
+//   flex-basis: 50%;
+//   height: 100vh;
+//   font-size: 48px;
+//   justify-content: center;
+//   align-items: center;
+//   box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px,
+//     rgba(0, 0, 0, 0.3) 0px 30px 60px -30px,
+//     rgba(10, 37, 64, 0.35) 0px -2px 6px 0px inset;
+// `;
+
+// export const Mbtn = styled.div`
+/* width: 99%;
+height: 99%; */
+// `;
 
 export const WrapMain = styled.div`
   width: 100%;
@@ -9,18 +35,43 @@ export const WrapMain = styled.div`
   align-items: center;
 `;
 
-export const WrapBtn = styled.div`
+export const WrapBtn = styled.button`
   display: flex;
   flex-basis: 50%;
+  width: 50vw;
   height: 100vh;
   font-size: 48px;
   justify-content: center;
   align-items: center;
-  box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px, rgba(10, 37, 64, 0.35) 0px -2px 6px 0px inset;
+  box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px,
+    rgba(0, 0, 0, 0.3) 0px 30px 60px -30px,
+    rgba(10, 37, 64, 0.35) 0px -2px 6px 0px inset;
+
+  transform-style: preserve-3d;
+  transition: transform 2s;
+
+  &:hover {
+    cursor: pointer;
+  }
+
+  &:first-child {
+    transform-origin: top right;
+  }
+
+  &:last-child {
+    transform-origin: top left;
+  }
 `;
 
-export const Mbtn = styled.div`
-/* width: 99%;
-height: 99%; */
+export const FlipWrapper = styled.div`
+  display: inline-block;
+  perspective: 1000px;
 
+  &:focus-within ${WrapBtn} {
+    transform: rotateY(-180deg);
+  }
+
+  &:focus-within ${WrapBtn}:first-child {
+    transition: color transparent 0.5s;
+  }
 `;
