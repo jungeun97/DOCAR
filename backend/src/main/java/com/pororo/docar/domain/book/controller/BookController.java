28df16417acdfee3c75781017b2951390427a71d
jungeun park <pyooooong@gmail.com>
2023-04-25 15:55:11 +0900

    feat: 알라딘 Open API 호출 코드 작성

--- backend/src/main/java/com/pororo/docar/domain/book/controller/BookController.java
+++ backend/src/main/java/com/pororo/docar/domain/book/controller/BookController.java
@@ -1,13 +1,32 @@
 package com.pororo.docar.domain.book.controller;
 
+import com.pororo.docar.domain.book.service.BookApiResponse;
 import com.pororo.docar.domain.book.service.BookService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Tag(name = "도서 관리")
 @RestController
-@RequiredArgsConstructor
+//@RequiredArgsConstructor
+@RequestMapping("/books")
 public class BookController {
     private final BookService bookService;
+
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @PostMapping("/fetch")
+    public ResponseEntity<Void> fetchDataFromApi() {
+        bookService.fetchDataFromApi();
+        return ResponseEntity.ok().build();
+    }
+
+
+
+
 }
