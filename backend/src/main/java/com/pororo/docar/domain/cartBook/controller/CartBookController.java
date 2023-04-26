1944b30ff1d8e04242a86bff8c607ac3bd694f3e
최종현 <chn9801@naver.com>
2023-04-26 13:09:29 +0900

    feat(BE): 카트에 있는 책 정리 api

--- backend/src/main/java/com/pororo/docar/domain/cartBook/controller/CartBookController.java
+++ backend/src/main/java/com/pororo/docar/domain/cartBook/controller/CartBookController.java
@@ -3,15 +3,14 @@ package com.pororo.docar.domain.cartBook.controller;
 import com.pororo.docar.common.dto.ApiResponse;
 import com.pororo.docar.domain.cartBook.dto.BookSetList;
 import com.pororo.docar.domain.cartBook.service.CartBookService;
+import com.pororo.docar.domain.tmpBook.dto.TmpBookInfo;
 import com.pororo.docar.domain.user.dto.UserCreateRequest;
 import com.pororo.docar.domain.user.dto.UserSimpleInfo;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,10 +20,25 @@ import java.util.List;
 public class CartBookController {
     private final CartBookService cartBookService;
 
-    @GetMapping("cartbook/all")
+    @GetMapping("/cartbooks")
     public ResponseEntity<ApiResponse<List<BookSetList>>> getBooks() {
         List<BookSetList> bookSetList = cartBookService.getBooks();
         ApiResponse<List<BookSetList>> result = new ApiResponse<>(true, "책 정리 리스트", bookSetList);
+        return new ResponseEntity<>(result, HttpStatus.OK);
+    }
+
+//    @DeleteMapping("/cartbooks")
+//    public ResponseEntity<ApiResponse> deleteCartBooks() {
+//        cartBookService.deleteSetBooks();
+//        ApiResponse result = new ApiResponse(true, "다음 책장으로 이동하세요");
+//        return new ResponseEntity<>(result, HttpStatus.OK);
+//    }
+
+    @PostMapping("/cartbooks")
+    public ResponseEntity<ApiResponse<List<BookSetList>>> setBooks() {
+        List<BookSetList> bookSetLists = cartBookService.getBooksByBookshelf();
+        ApiResponse<List<BookSetList>> result = new ApiResponse<>(true, "정리를 시작하세요", bookSetLists);
         return new ResponseEntity<>(result, HttpStatus.CREATED);
     }
+
 }
--- backend/src/main/java/com/pororo/docar/domain/cartBook/service/CartBookService.java
+++ backend/src/main/java/com/pororo/docar/domain/cartBook/service/CartBookService.java
@@ -3,18 +3,29 @@ package com.pororo.docar.domain.cartBook.service;
 import com.pororo.docar.domain.cartBook.dto.BookSetList;
 import com.pororo.docar.domain.cartBook.entity.CartBook;
 import com.pororo.docar.domain.cartBook.repository.CartBookRepository;
+import com.pororo.docar.domain.tmpBook.entity.TmpBook;
+import com.pororo.docar.domain.tmpBook.repository.TmpBookRepository;
+import com.pororo.docar.domain.tmpBook.service.TmpBookService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+
 
 @Service
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class CartBookService {
     private final CartBookRepository cartBookRepository;
+    private final TmpBookRepository tmpBookRepository;
+    private final TmpBookService tmpBookService;
+
+//    public List<Long> orderList = tmpBookService.orderList;
+    public static List<Long> orderList = new LinkedList<>();
+
 
     public List<BookSetList> getBooks() {
         List<BookSetList> list = new ArrayList<>();
@@ -23,7 +34,74 @@ public class CartBookService {
         for (CartBook cartBook : bookList) {
             list.add(new BookSetList().entityToDto(cartBook));
         }
-
         return list;
     }
+
+
+
+    @Transactional
+    public List<BookSetList> getBooksByBookshelf() {
+        List<TmpBook> list = new ArrayList<>();
+        orderList.add(1L);
+        orderList.add(2L);
+        Long idx = orderList.remove(0);
+
+        System.out.println(idx);
+        List<CartBook> bookList = cartBookRepository.findAll();
+        for (CartBook cartBook : bookList) {
+            if (cartBook.getBook().getBookshelf().getId() == idx) {
+                TmpBook tmpBook = TmpBook.builder()
+                        .floor(cartBook.getFloor())
+                        .site(cartBook.getSite())
+                        .book(cartBook.getBook())
+                        .build();
+                list.add(tmpBook);
+            }
+        }
+        System.out.println(list);
+        tmpBookRepository.saveAll(list);
+
+        List<BookSetList> setList = new ArrayList<>();
+        List<TmpBook> setBookList = tmpBookRepository.findAll();
+        System.out.println(setBookList);
+
+        for (TmpBook tmpBook : setBookList) {
+            CartBook cartBook = CartBook.builder()
+                    .floor(tmpBook.getFloor())
+                    .site(tmpBook.getSite())
+                    .book(tmpBook.getBook())
+                    .build();
+            System.out.println(cartBook);
+            setList.add(new BookSetList().entityToDto(cartBook));
+        }
+        System.out.println(setList);
+        return setList;
+    }
+
+//    @Transactional
+//    public void deleteSetBooks() {
+//        List<TmpBook> setBookList = tmpBookRepository.findAll();
+//        List<CartBook> doneBookList = cartBookRepository.findAll();
+//
+//        for (TmpBook tmpBook : setBookList) {
+//            for (CartBook cartBook : doneBookList) {
+//                Long id = tmpBook.getBook().getId();
+//                if (id == cartBook.getBook().getId()) {
+//                    cartBookRepository.delete(cartBook);
+//                }
+//            }
+//        }
+//        tmpBookRepository.deleteAll();
+//    }
+
+
+
+    ///////////////////////////////
+
+    public CartBook getBookById(Long id) {
+        return cartBookRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Id를 확인해주세요."));
+    }
+
+
 }
