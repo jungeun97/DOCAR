5a920b77c2cda2f8a84e8b3f3e99dbd24bc23a93
haetsal <leeee99@hanmail.net>
2023-04-27 09:51:05 +0900

    feat: API 기본 세팅

--- frontend/src/store/api.ts
+++ frontend/src/store/api.ts
@@ -1,34 +1,38 @@
-import axios, { AxiosInstance } from 'axios';
+import axios, { AxiosError } from 'axios';
 
-const API_URL = 'https://k8d101.p.ssafy.io/api/';
+export interface ReturnList {
+  id: number;
+  floor: number;
+  site: number;
+  bookName: string;
+}
 
-const API: AxiosInstance = axios.create({
-  baseURL: API_URL,
-  withCredentials: true,
-  headers: {
-    'Content-Type': 'application/json;charset=UTF-8',
-    accept: 'application/json,',
-  },
-});
+const API_URL = 'http://k8d101.p.ssafy.io:8080/api';
 
-// API.interceptors.request.use((config) => {
-//   const accessToken = Cookies.get('accessToken');
-//   if (accessToken) {
-//     config.headers.Authorization = `Bearer ${accessToken}`;
-//   }
-//   return config;
-// });
-
-API.interceptors.response.use(
-  (response) => {
-    return response;
-  },
-  async (error) => {
-    const responseStatus = error?.response?.status || null;
-    // console.log(`Error: ${responseStatus}`);
-    // Handle error cases
-    return Promise.reject(error);
+function printError(error: AxiosError) {
+  if (error.response) {
+    // console.log(error.response.data);
+  } else if (error.message) {
+    // console.log('오류 메시지:', error.message);
+  } else {
+    // console.log('알 수 없는 오류가 발생했습니다.');
   }
-);
+}
 
-export default API;
+// 반납 책 리스트 조회
+export async function getReturnList(): Promise<ReturnList | null> {
+  let data: ReturnList | null = null;
+
+  try {
+    const res = await axios.get(`${API_URL}/returns`, {
+      withCredentials: true,
+    });
+
+    data = res.data.data as ReturnList;
+  } catch (error) {
+    const axiosError = error as AxiosError;
+    // printError(axiosError);
+  }
+
+  return data;
+}
