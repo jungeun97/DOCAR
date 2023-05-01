426fd556853acf0d1bd57a70a827d7c2b234f108
kavin816@naver.com <kavin816@naver.com>
2023-05-01 09:49:17 +0900

    fix: swagger 요청 주소 변경

--- backend/src/main/java/com/pororo/docar/DocarApplication.java
+++ backend/src/main/java/com/pororo/docar/DocarApplication.java
@@ -1,8 +1,11 @@
 package com.pororo.docar;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(servers = {@Server(url = "https://k8d101.p.ssafy.io/api", description = "Generated Server URL")})
 @SpringBootApplication
 public class DocarApplication {
 
