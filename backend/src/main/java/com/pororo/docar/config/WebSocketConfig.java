f0a913b33eaf9a4dcef33e72a3379e6f8b7812f9
kavin816@naver.com <kavin816@naver.com>
2023-05-03 14:10:32 +0900

    feat: handshake handler 작성

--- backend/src/main/java/com/pororo/docar/config/WebSocketConfig.java
+++ backend/src/main/java/com/pororo/docar/config/WebSocketConfig.java
@@ -1,5 +1,6 @@
 package com.pororo.docar.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
@@ -7,13 +8,18 @@ import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
+import org.springframework.web.socket.server.HandshakeHandler;
+import org.springframework.web.socket.server.support.DefaultHandshakeHandler;
+import org.springframework.web.socket.server.support.HttpSessionHandshakeInterceptor;
 
 @Configuration
 @EnableWebSocket
 public class WebSocketConfig implements WebSocketConfigurer {
+
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(new MyWebSocketHandler(), "/ws").setAllowedOrigins("*");
+        registry.addHandler(new MyWebSocketHandler(), "/ws").setAllowedOrigins("*")
+                .addInterceptors(new HttpSessionHandshakeInterceptor());;
     }
 
     static class MyWebSocketHandler extends TextWebSocketHandler {
@@ -27,5 +33,10 @@ public class WebSocketConfig implements WebSocketConfigurer {
             System.out.println("WebSocket 메시지 수신: " + message.getPayload());
             session.sendMessage(new TextMessage("WebSocket 응답: " + message.getPayload()));
         }
+    }
+
+    @Bean
+    public HandshakeHandler handshakeHandler() {
+        return new DefaultHandshakeHandler();
     }
 }
