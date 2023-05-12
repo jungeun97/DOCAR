f2086e99f6f0ae036d68efc9dd67e8ce56faa722
kavin816@naver.com <kavin816@naver.com>
2023-05-12 16:37:49 +0900

    refactor: 소켓통신 DTO 어노테이션 방식 사용

--- backend/src/main/java/com/pororo/docar/config/dto/SensorInfo.java
+++ backend/src/main/java/com/pororo/docar/config/dto/SensorInfo.java
@@ -1,13 +1,16 @@
 package com.pororo.docar.config.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
-@AllArgsConstructor
 public class SensorInfo {
     private long distance;
     private String barcode;
+
+    public SensorInfo(long distance, String barcode) {
+        this.distance = distance;
+        this.barcode = barcode;
+    }
 }
